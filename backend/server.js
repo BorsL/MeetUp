af07a7f73876937edd0ce595ce31cec6fb625a3d
BorsL <126157610+BorsL@users.noreply.github.com>
2025-02-11 18:49:21 +0100

    last revisit

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import { connectDB } from "./config/db.js";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from 'cors'
+import path from 'path';
 
 import productRoutes from './routes/products.route.js'
 import authRoutes from './routes/auth.route.js'
@@ -11,6 +12,8 @@ dotenv.config()
 
 const app = express()
 const PORT = process.env.PORT || 5002
+
+const __dirname = path.resolve();
 
 app.use(express.json()); //allows us to accept json on body
 app.use(cookieParser());
@@ -21,6 +24,16 @@ app.use(cors({
 
 app.use('/api/products', productRoutes)
 app.use('/api/auth', authRoutes)
+
+
+//this will fix problem to different port between production
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname, "/frontend/dist")))
+
+    app.get('*', (req,res)=>{
+        res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+    })
+}
 
 app.listen(PORT, ()=>{
     connectDB();
